a0ab810b8be834ed792adebf3a355804ad634d20
Fallingevil <denjikun810@gmail.com>
2024-05-04 16:43:11 +0500

    modified

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Oyatullo'};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome my lovely"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Accesss and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
