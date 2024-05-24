9c2c43693b7074e52d754040e397b485554e1fa1
Fallingevil <denjikun810@gmail.com>
2024-05-24 12:49:40 +0500

    Fix typo in Totalbalance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,23 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: 'Oyatullo'};
+  const loggedIn = {
+    firstName: "Oyatullo",
+    lastName: "Juraev",
+    email: "example@gamil.com",
+  };
 
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
-          <HeaderBox 
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
             type="greeting"
             title="Welcome my lovely"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Accesss and manage your account and transactions efficiently"
           />
 
@@ -22,9 +27,16 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
-    </section>
-  )
-}
 
-export default Home
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.45 }, { currentBalance: 678.9 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
