32b37228979302303315697587c6f5778d69cac9
Fallingevil <denjikun810@gmail.com>
2024-05-28 18:33:26 +0500

    added footer without logout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Oyatullo",
-    lastName: "Juraev",
-    email: "example@gamil.com",
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className="home">
@@ -17,7 +14,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome my lovely"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Accesss and manage your account and transactions efficiently"
           />
 
